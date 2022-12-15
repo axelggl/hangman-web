3c47b1a1c908e182404e3001891b40066ce3e19f
Nils <nils.jaudon@ynov.com>
2022-12-15 12:21:54 +0100

    test

--- main.go
+++ main.go
@@ -2,44 +2,28 @@ package main
 
 import (
 	"html/template"
-	"log"
 	"net/http"
 )
 
-var tmpl *template.Template
-
-type Todo struct {
-	Item string
-	Done bool
+type Page struct {
+	Valeur string
 }
 
-type PageData struct {
-	Title string
-	Todos []Todo
-}
-
-func todo(w http.ResponseWriter, r *http.Request) {
-
-	data := PageData{
-		Title: "TODO List",
-		Todos: []Todo{
-			{Item: "Install GO", Done: true},
-			{Item: "Learn Go", Done: false},
-			{Item: "Like this video", Done: false},
-		},
-	}
-
-	tmpl.Execute(w, data)
-
-}
+var templates = template.Must(template.ParseFiles("index.html"))
 
 func main() {
-	mux := http.NewServeMux()
-	tmpl = template.Must(template.ParseFiles("index.gohtml"))
+	http.HandleFunc("/home", homeHandler)
+	http.ListenAndServe(":9999", nil)
+}
 
-	fs := http.FileServer(http.Dir("./static"))
-	mux.Handle("/static/", http.StripPrefix("/static/", fs))
-	mux.HandleFunc("/todo", todo)
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	p := Page{Valeur: "A"}
+	err := templates.ExecuteTemplate(w, "index.html", p)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
 
-	log.Fatal(http.ListenAndServe(":9091", mux))
+func postApi(w http.ResponseWriter, r *http.Request) {
+
 }

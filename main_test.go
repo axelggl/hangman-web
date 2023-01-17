52ab0e979037d66673bb0840ded9acc383382bd7
Nils <nils.jaudon@ynov.com>
2023-01-17 17:32:11 +0100

    test

---
+++ main_test.go
@@ -0,0 +1,16 @@
+package main
+
+/*
+func Test_helloServer(t *testing.T) {
+	request, _ := http.NewRequest(http.MethodGet, "/Foo", nil)
+	response := httptest.NewRecorder()
+
+	homeHandler(response, request)
+
+	got := response.Body.String()
+	want := "Hello, Foo!"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+*/

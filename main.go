65ef4ec249e4334eeb506e2b09a17bf99d67a03f
Nils <nils.jaudon@ynov.com>
2023-01-17 13:49:30 +0100

    terst

--- main.go
+++ main.go
@@ -15,24 +15,24 @@ type Page struct {
 var templates = template.Must(template.ParseFiles("index.html")) // utiliser pour parser (analyse le contenu du fichier)
 
 func main() { // gestion URL
-	hangman_classic.Clear()
+	fmt.Println("bonjour")
+	u := openWordsList()
+	test := hangman_classic.CreateWord(u)
+	fmt.Println(test)
 
-	fmt.Println("bonjours")
-	http.HandleFunc("/home", homeHandler) // lui dit : L'url pour envoyer la req—uette
-	http.ListenAndServe(":8080", nil)     // demarre un serveur http sur le port 8080
+	http.HandleFunc("/home", homeHandler) // utilise func homeHandler pour accèder a l'url /home
+	http.ListenAndServe(":8080", nil)     // ecoute sur le port 8080,  est un gestionnaire de route
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) { // envoie une requête a /home, et crée avec la struc Page et est modif avec A
-	word := openWordsList()
-	word_hide := hangman_classic.CreateWord(word)
-	p := Page{Valeur: word_hide}
+func homeHandler(w http.ResponseWriter, r *http.Request) { // gère les routes de l'application en indiquant à l'application que lorsque l'utilisateur accède à l'URL "/home", la fonction homeHandler doit être utilisée pour gérer sa demande.
 
+	p := Page{Valeur: hangman_classic.CreateWord(openWordsList())}
 	err := templates.ExecuteTemplate(w, "index.html", p) // prépare la réponse
 	if err != nil {
 
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		fmt.Fprintf(w, "ParseForm() err: %v", err) // gere les erreurs
 	}
-	name := r.FormValue("name") // attribue la variable au formulaire name
-	fmt.Println(name)
+	Letter := r.FormValue("Letter") // attribue la variable au formulaire name
+	fmt.Println(Letter)
 }
